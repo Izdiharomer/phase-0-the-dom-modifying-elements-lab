dfc180f6378edbda534164c92a568338c46aa2a6
Izdihar <ezdiharomerrrr@gmail.com>
2023-04-29 23:51:16 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-// Write your code here!
+const main = document.getElementById("main");
+main.remove()
+
+const newHeader = document.createElement('h1');
+document.body.append(newHeader)
+
+newHeader.setAttribute('id', "victory");
+
+newHeader.id= 'victory'
+newHeader.innerHTML = 'Izdihar is the champion';
